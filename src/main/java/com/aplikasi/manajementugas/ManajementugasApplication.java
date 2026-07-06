40796754a65e07ce33923999770cab5c1a57b2fc
AplikasiUser <ariotanggu10092002@gmail.com>
2026-07-06 17:58:45 +0800

    tugas pertama

---
+++ src/main/java/com/aplikasi/manajementugas/ManajementugasApplication.java
@@ -0,0 +1,13 @@
+package com.aplikasi.manajementugas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajementugasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajementugasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplikasi/manajementugas/ManajementugasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplikasi.manajementugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajementugasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
